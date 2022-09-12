b47f1232ec4850d56de8c91b7a4e123c44527352
dreth <github@dac.ac>
2022-09-12 23:43:00 +0200

    add language cli argument and remove data without month/day

--- check_bdays.py
+++ check_bdays.py
@@ -5,6 +5,29 @@ from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
 import datetime as dt
+import argparse
+import json
+
+# parse cli arguments
+parser = argparse.ArgumentParser(description="Send reminder birthday messages to telegram chat id if there's a birthday", formatter_class=argparse.RawTextHelpFormatter)
+
+# language flag
+parser.add_argument('-l', '--language', type=str, default="en", help="Language to use for the bot as an ISO 3166-1 alpha-2\n language code. Default is 'en' (English), not case sensitive.\n If the code is not present in the keys of 'language.json'\n you can translate it yourself and (ideally) submit a PR 😁:\n https://github.com/dreth/BdayReminderBot.")
+
+# parse arguments
+args = parser.parse_args()
+
+# use args
+if args.language:
+    # load languages
+    with open('languages.json') as f:
+        LANGUAGES = json.load(f)
+
+    # check if lang exists, otherwise just use 'en'
+    args.language = 'en' if args.language not in LANGUAGES.keys() else args.language
+    
+    # set messages' language
+    sentence = [LANGUAGES[args.language.lower()][x] for x in range(3)]
 
 # load env vars
 load_dotenv()
@@ -20,23 +43,27 @@ def prepare_reminder_message(bdays_df):
     # loop over bdays df and append the message if the person has a bday today
     for i in range(bdays_df.shape[0]):
         if ~np.isnan(bdays_df.year[i]):
-            messages.append(f"Today is {bdays_df.name[i]}'s birthday - They turn {TODAY.year - bdays_df.year[i]} today")
+            messages.append(sentence[0].format(bdays_df.name[i],int(TODAY.year - bdays_df.year[i])))
         else:
-            messages.append(f"Today is {bdays_df.name[i]}'s birthday")
+            messages.append(sentence[1].format(bdays_df.name[i]))
 
     # return the messages
-    return '\n'.join(messages)
+    return sentence[2].format(TODAY) + '\n'.join(messages)
 
 # bday checker
 def check_bdays():
     # read data
     df = pd.read_csv("birthday_list.csv")
 
+    # only keep elements that have at least month and day
+    df = df[df["month"].notna() & df["day"].notna()]
+
     # parse dates in the dataset
-    df["date"] = [dt.date(TODAY.year,m,d) for m, d in zip(df["month"], df["day"])]
+    df["date"] = [dt.date(TODAY.year,int(m),int(d)) for m, d in zip(df["month"], df["day"])]
 
     # return who's bday is today
     return df[df["date"] == TODAY]
+
 
 # main
 async def main():
@@ -47,9 +74,6 @@ async def main():
     birthdays_message = prepare_reminder_message(check_bdays())
 
     async with bot:
-        # send "bdays for today" message
-        await bot.send_message(text=f"Birthdays for today: {TODAY}", chat_id=YOUR_CHAT_ID)
-
         # send birthdays message
         await bot.send_message(text=birthdays_message, chat_id=YOUR_CHAT_ID)
 
