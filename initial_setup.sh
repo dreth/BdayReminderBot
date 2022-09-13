9eed505e3a40ab0d808ae68246a5ed31dab51ad0
dreth <github@dac.ac>
2022-09-13 15:34:43 +0200

    im dumb, i forgot it opens a new bash console

--- initial_setup.sh
+++ initial_setup.sh
@@ -6,4 +6,3 @@ pip install -r requirements.txt
 pip install python-telegram-bot -U --pre
 echo -e "TELEGRAM_BOT_API_KEY=\"APIKEY\"\nYOUR_CHAT_ID=\"CHATID\"" > .env && rm .env.template
 echo -e "name,year,month,day\n" > birthday_list.csv 
-source ./bdaychecker/bin/activate
